7b400d9f23a42781f7f9b5a67ccc7fae500838cf
Stefan Freitag <stefan.freitag@udo.edu>
2020-05-09 21:43:31 +0200

    chore: Upgrade to CDK 1.38.0 (#9)
    Added information on CloudFront

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # CI/ CD Pipeline for an Angular project
 
-## Source code repository
+The [AWS Cloud  Development Kit](https://github.com/aws/aws-cdk) (CDK) is used to build and expose an [Angular](https://angular.io/) application using [AWS CloudFront](https://aws.amazon.com/cloudfront/).
+
+## The source code
 
 The Angular source code is stored in [one of my GitHub repositories](https://github.com/stefanfreitag/angular-hello-world). It is a very basic application that can be build by executing
 
@@ -8,7 +10,7 @@ The Angular source code is stored in [one of my GitHub repositories](https://git
 ng build --prod
 ```
 
-## Build pipeline
+## The build pipeline
 
 The build pipeline consists of two stages, the `sourceStage` and the `buildStage`.
 
@@ -41,7 +43,7 @@ const sourceAction = new GitHubSourceAction({
 sourceStage.addAction(sourceAction);
 ```
 
-In the `buildstage` a CodeBuild project is exectued. As part of the project the required software is installed: all required libraries, the AWS as well as the Angular CLI.
+In the `buildstage` a CodeBuild project is executed. As part of the project the required software is installed: all required libraries, the AWS as well as the Angular CLI.
 
 ```bash
 npm install
@@ -56,4 +58,54 @@ The last phase is responsible for updating the content of the used S3 bucket.
 ```bash
 aws s3 rm s3://${websiteBucket.bucketName}/ --recursive
 aws s3 cp ./dist/angular-hello-world s3://${websiteBucket.bucketName}/ --recursive
+```
+
+## The CloudFront setup
+
+The S3 bucket `websiteBucket` is not exposed directly to the Internet, CloudFront will be installed in between. Hence the `publicReadAccess`  and `blockPublicAccess` are set to `false` settings.
+
+```javascript
+const websiteBucket = new Bucket(this, "bucket", {
+  websiteIndexDocument: "index.html",
+  publicReadAccess: false,
+  encryption: BucketEncryption.S3_MANAGED,
+  blockPublicAccess: {
+    restrictPublicBuckets: false,
+    blockPublicAcls: false,
+    ignorePublicAcls: false,
+    blockPublicPolicy: false,
+  },
+   removalPolicy: RemovalPolicy.DESTROY,
+});
+```
+
+To allow CloudFront access to the content of the S3 bucket a user is setup
+
+```javascript
+const oai = new OriginAccessIdentity(this, "originAccessIdentity", {
+  comment: "Allows CloudFront to reach the bucket",
+});
+websiteBucket.grantRead(oai);
+```
+
+The `CloudFrontWebDistribution` contains aside from the `websiteBucket` and the user information also another bucket used to store the access logs.
+
+```javascript
+new CloudFrontWebDistribution(this, "myDistribution", {
+  originConfigs: [
+    {
+      s3OriginSource: {
+        s3BucketSource: websiteBucket,
+        originAccessIdentity: oai,
+      },
+      behaviors: [{ isDefaultBehavior: true }],
+    }],
+    comment: "Demo CloudFront Distribution",
+    loggingConfig: {
+      bucket: accessLogsBucket,
+      prefix: "ng-s3-demo",
+      includeCookies: true,
+    },
+    priceClass: PriceClass.PRICE_CLASS_100,
+});
 ```
--- lib/ng-s3-ci-cd-stack.ts
+++ lib/ng-s3-ci-cd-stack.ts
@@ -1,79 +1,102 @@
 import * as cdk from "@aws-cdk/core";
-import { Bucket, BucketPolicy } from "@aws-cdk/aws-s3";
+import {
+  Bucket,
+  BucketPolicy,
+  BucketEncryption,
+  LifecycleRule,
+} from "@aws-cdk/aws-s3";
 import { Pipeline, Artifact } from "@aws-cdk/aws-codepipeline";
+import {
+  CloudFrontWebDistribution,
+  PriceClass,
+  OriginAccessIdentity,
+} from "@aws-cdk/aws-cloudfront";
 import {
   GitHubSourceAction,
   GitHubTrigger,
-  CodeBuildAction
+  CodeBuildAction,
 } from "@aws-cdk/aws-codepipeline-actions";
-import { SecretValue } from "@aws-cdk/core";
+import { SecretValue, RemovalPolicy, Duration, Tag } from "@aws-cdk/core";
 import { BuildSpec, Project } from "@aws-cdk/aws-codebuild";
-import {
-  Role,
-  ServicePrincipal,
-  ManagedPolicy,
-  Effect,
-  PolicyStatement,
-  AnyPrincipal
-} from "@aws-cdk/aws-iam";
+import { Role, ServicePrincipal, ManagedPolicy } from "@aws-cdk/aws-iam";
 
 export class NgS3CiCdStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const websiteBucket = new Bucket(this, "bucket", {
-      websiteIndexDocument: "index.html",
+    /**
+     * S3 bucket storing the access logs for the web site.
+     */
+    const accessLogsBucket = new Bucket(this, "accessLogsBucket", {
+      encryption: BucketEncryption.S3_MANAGED,
       publicReadAccess: false,
       blockPublicAccess: {
         restrictPublicBuckets: false,
         blockPublicAcls: false,
         ignorePublicAcls: false,
-        blockPublicPolicy: false
-      }
+        blockPublicPolicy: false,
+      },
+      removalPolicy: RemovalPolicy.DESTROY,
+      lifecycleRules: [
+        {
+          enabled: true,
+          expiration: Duration.days(14),
+        },
+      ],
     });
 
-    const bucketContentStatement = new PolicyStatement({
-      effect: Effect.ALLOW,
-      actions: ["s3:GetObject"],
-      resources: [websiteBucket.bucketArn + "/*"],
-      principals: [new AnyPrincipal()]
-    });
-    bucketContentStatement.addCondition("IpAddress", {
-      "aws:SourceIp": "87.122.209.106/32"
+    const oai = new OriginAccessIdentity(this, "originAccessIdentity", {
+      comment: "Allows CloudFront to reach the bucket",
     });
 
-    const bucketStatement: PolicyStatement = new PolicyStatement({
-      effect: Effect.ALLOW,
-      actions: ["s3:ListBucket", "s3:GetBucketLocation"],
-      resources: [websiteBucket.bucketArn],
-      principals: [new AnyPrincipal()]
-    });
-    bucketStatement.addCondition("IpAddress", {
-      "aws:SourceIp": "87.122.209.106/32"
+    const websiteBucket = new Bucket(this, "webSiteBucket", {
+      websiteIndexDocument: "index.html",
+      publicReadAccess: false,
+
+      encryption: BucketEncryption.S3_MANAGED,
+      blockPublicAccess: {
+        restrictPublicBuckets: false,
+        blockPublicAcls: false,
+        ignorePublicAcls: false,
+        blockPublicPolicy: false,
+      },
+      removalPolicy: RemovalPolicy.DESTROY,
     });
 
-    const bucketPolicy = new BucketPolicy(this, "bucketPolicy", {
-      bucket: websiteBucket
-    });
+    websiteBucket.grantRead(oai);
 
-    bucketPolicy.document.addStatements(
-      bucketContentStatement,
-      bucketStatement
-    );
+    new CloudFrontWebDistribution(this, "myDistribution", {
+      originConfigs: [
+        {
+          s3OriginSource: {
+            s3BucketSource: websiteBucket,
+            originAccessIdentity: oai,
+          },
+          behaviors: [{ isDefaultBehavior: true }],
+        },
+      ],
+      comment: "Demo CloudFront Distribution",
+      loggingConfig: {
+        bucket: accessLogsBucket,
+        prefix: "ng-s3-demo",
+        includeCookies: true,
+      },
+      priceClass: PriceClass.PRICE_CLASS_100,
+    });
 
     const pipeline = new Pipeline(this, "FrontendPipeline", {
-      pipelineName: "deploy-angular-application"
+      pipelineName: "deploy-angular-application",
     });
 
     const sourceStage = pipeline.addStage({
-      stageName: "Source"
+      stageName: "Source",
     });
 
     const buildStage = pipeline.addStage({
       stageName: "Build",
       placement: {
-        justAfter: sourceStage
-      }
+        justAfter: sourceStage,
+      },
     });
 
     const sourceOutput = new Artifact();
@@ -84,15 +107,15 @@ export class NgS3CiCdStack extends cdk.Stack {
       oauthToken: SecretValue.secretsManager("my-github-token"),
       output: sourceOutput,
       branch: "master",
-      trigger: GitHubTrigger.POLL
+      trigger: GitHubTrigger.POLL,
     });
     sourceStage.addAction(sourceAction);
 
     const role = new Role(this, "CodeBuildRole", {
       assumedBy: new ServicePrincipal("codebuild.amazonaws.com"),
       managedPolicies: [
-        ManagedPolicy.fromAwsManagedPolicyName("AmazonS3FullAccess")
-      ]
+        ManagedPolicy.fromAwsManagedPolicyName("AmazonS3FullAccess"),
+      ],
     });
 
     const codeBuild = new Project(this, "CodeBuildProject", {
@@ -102,7 +125,7 @@ export class NgS3CiCdStack extends cdk.Stack {
         phases: {
           install: {
             "runtime-versions": {
-              nodejs: 10
+              nodejs: 10,
             },
             commands: [
               "echo installing dependencies",
@@ -112,36 +135,36 @@ export class NgS3CiCdStack extends cdk.Stack {
               "echo check version",
               "aws --version",
               "echo installing angular cli",
-              "npm i -g @angular/cli"
-            ]
+              "npm i -g @angular/cli",
+            ],
           },
           build: {
             commands: [
               "echo Build started on `date`",
               "echo Building angular-hello-world",
-              "ng build --prod"
+              "ng build --prod",
             ],
             artifacts: {
               files: ["**/*"],
               "base-directory": "dist/angular-hello-world",
-              "discard-paths": "yes"
-            }
+              "discard-paths": "yes",
+            },
           },
           post_build: {
             commands: [
               "echo BUILD COMPLETE running sync with s3",
               `aws s3 rm s3://${websiteBucket.bucketName}/ --recursive`,
-              `aws s3 cp ./dist/angular-hello-world s3://${websiteBucket.bucketName}/ --recursive`
-            ]
-          }
-        }
-      })
+              `aws s3 cp ./dist/angular-hello-world s3://${websiteBucket.bucketName}/ --recursive`,
+            ],
+          },
+        },
+      }),
     });
 
     const buildAction = new CodeBuildAction({
       actionName: "Build",
       input: sourceOutput,
-      project: codeBuild
+      project: codeBuild,
     });
 
     buildStage.addAction(buildAction);
