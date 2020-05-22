03fc12c7f2207c582358d20acbc69ee114b55eda
Stefan Freitag <stefan.freitag@udo.edu>
2020-05-22 22:15:31 +0200

    feat: Added tests

--- test/ng-s3-ci-cd.test.ts
+++ test/ng-s3-ci-cd.test.ts
@@ -1,13 +1,47 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
-import * as cdk from '@aws-cdk/core';
-import NgS3CiCd = require('../lib/ng-s3-ci-cd-stack');
+import {
+  expect as expectCDK,
+  matchTemplate,
+  MatchStyle,
+  haveResource,
+} from "@aws-cdk/assert";
+import * as cdk from "@aws-cdk/core";
+import NgS3CiCd = require("../lib/ng-s3-ci-cd-stack");
+import { Stack } from "@aws-cdk/core";
+import { NgS3CiCdStack } from "../lib/ng-s3-ci-cd-stack";
 
-test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new NgS3CiCd.NgS3CiCdStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+test("S3 buckets are not public accessible ", () => {
+  const app = new cdk.App();
+
+  const stack = new NgS3CiCd.NgS3CiCdStack(app, "MyTestStack");
+
+  expectCDK(stack).to(
+    haveResource("AWS::S3::Bucket", {
+      PublicAccessBlockConfiguration: {
+        BlockPublicAcls: true,
+        BlockPublicPolicy: true,
+        IgnorePublicAcls: true,
+        RestrictPublicBuckets: true,
+      },
+    })
+  );
+});
+
+test("S3 buckets are encrypted ", () => {
+  const app = new cdk.App();
+
+  const stack = new NgS3CiCd.NgS3CiCdStack(app, "MyTestStack");
+
+  expectCDK(stack).to(
+    haveResource("AWS::S3::Bucket", {
+      BucketEncryption: {
+        ServerSideEncryptionConfiguration: [
+          {
+            ServerSideEncryptionByDefault: {
+              SSEAlgorithm: "AES256",
+            },
+          },
+        ],
+      },
+    })
+  );
 });
